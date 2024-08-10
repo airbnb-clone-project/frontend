057532367ca10ef967771fbee79c8fc18f3fe948
bomlang <bomlang4211@gmail.com>
2024-08-10 15:43:25 +0900

    chore: initial project setup with tailwind

---
+++ app/providers.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  dehydrate,
+  HydrationBoundary,
+} from "@tanstack/react-query";
+import { useState } from "react";
+import React from "react";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000, // 1분
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        {children}
+      </HydrationBoundary>
+    </QueryClientProvider>
+  );
+};
+
+export default Providers;
